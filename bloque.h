3f24a73f3da2d5e93fe5778061a87af49536a6da
RampantBag43488 <inakimancera@gmail.com>
2025-06-12 07:49:36 -0600

    Update comentarios para bloque.h

--- bloque.h
+++ bloque.h
@@ -1,12 +1,27 @@
+/*
+ *
+ * Proyecto inventario clase Bloque
+ * Inaki Mancera Llano
+ * A01708827
+ * 12/06/2025
+ * version: 1
+ * Esta clase defina objeto de tipo Item que contiene la clase heredada
+ * Bloque.
+ */
+
 #ifndef BLOQUE_H
 #define BLOQUE_H
 
 #include "item.h"
 
+//Declaracion del objeto herramienta que hereda de Item
 class Bloque : public Item {
+
+//Variables de instancia del objeto
 private:
     bool Tipo; // true = Construccion, false = naturaleza 
 
+//Metodos del objeto
 public:
     Bloque();
     Bloque(string nombre, bool t, bool s);
@@ -25,6 +40,14 @@ string Bloque::getTipo() {
     return Tipo ? "Construccion" : "Naturaleza"; 
 }
 
+/**
+ * verItem muestra la informacion general del bloque en cadena de string.
+ *
+ * concatena todos los valores de los atributos en un string para ser impreso
+ *
+ * @param
+ * @return imprime un string con los valores y texto concatenado.
+ */
 void Bloque::verItem(){
     cout << "\nEl bloque " << Nombre << " tiene las siguintes caracteristicas: " << endl;
     if (Stack == 1){
