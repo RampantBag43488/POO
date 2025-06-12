e890aaeeb659f906eafeda5f96e90de4ec6a4e26
RampantBag43488 <inakimancera@gmail.com>
2025-06-12 07:53:02 -0600

    Update comentarios para consumible.h

--- consumible.h
+++ consumible.h
@@ -1,13 +1,28 @@
+/*
+ *
+ * Proyecto inventario clase Consumible
+ * Inaki Mancera Llano
+ * A01708827
+ * 12/06/2025
+ * version: 1
+ * Esta clase defina objeto de tipo Item que contiene la clase heredada
+ * Consumible.
+ */
+
 #ifndef CONSUMIBLE_H
 #define CONSUMIBLE_H
 
 #include "item.h"
 
+//Declaracion del objeto consumible que hereda de Item
 class Consumible : public Item {
+
+//Variables de instancia del objeto
 private:
     int Vida;
     int Comida;
 
+//Metodos del objeto
 public:
     Consumible();
     Consumible(string nombre, int v, int c, bool s);
@@ -34,6 +49,14 @@ int Consumible::getComida() {
     return Comida; 
 }
 
+/**
+ * verItem muestra la informacion general del consumible en cadena de string.
+ *
+ * concatena todos los valores de los atributos en un string para ser impreso
+ *
+ * @param
+ * @return imprime un string con los valores y texto concatenado.
+ */
 void Consumible::verItem(){
     cout << "\nEl consumible " << Nombre << " tiene las siguintes caracteristicas: " << endl;
     if (Stack == 1){
